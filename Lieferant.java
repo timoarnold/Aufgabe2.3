ae3f25f0c699ccd005bb830ea94afa326a4d80d6
flolololol <florianne.walliser@student.unisg.ch>
2022-11-05 13:55:41 +0100

    Kommentare Lieferant

--- Lieferant.java
+++ Lieferant.java
@@ -6,11 +6,21 @@
  * Die Klasse Lieferant ermöglicht es Bestellungen an den Lieferanten aufzugeben.
  */
 
-
 public class Lieferant {
+    
     /**
-    * Instanzvariabeln?
-    *
+     * Instanzvariabeln: bisher keine.
+     */
+    
+    /**
+    * Bestellt benötigte Produktionsmaterialien beim Fabriklieferanten nach.
+    * 
+    * @param holzEinheiten: Holzeinheiten, welche im Lager gehalten & zur Produktion benötigt werden
+    * @param schrauben: Schrauben, welche im Lager gehalten & zur Produktion benötigt werden
+    * @param farbEinheiten: Farbeinheiten, welche im Lager gehalten & zur Produktion benötigt werden
+    * @param kartonEinheiten: Kartoneinheiten, welche im Lager gehalten & zur Produktion benötigt werden
+    * @param kissen: Kissen, welche im Lager gehalten & zur Produktion benötigt werden
+    * @return: Setze auf true, falls eine Bestellung vom Lager eingegangen ist.
     */
 
     public boolean wareBestellen (int holzEinheiten, int schrauben, int farbEinheiten,int kartonEinheiten,int kissen) {
