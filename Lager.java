d94cca84eb6d7fb6ee0b90b19d759772e7956568
timoarnold <115249192+timoarnold@users.noreply.github.com>
2022-12-11 11:27:49 +0100

    download lager.java separately

--- Lager.java
+++ Lager.java
@@ -1,7 +1,7 @@
 /**
  * @author Gruppe 29
  * @version 3.1 (4. Dezember 2022)
- * <p>
+ * 
  * Die Klasse Lager beinhaltet die Informationen zu den maximal lagerbaren Materialeinheiten sowie den aktuellen
  * Beständen der Materialien.
  */
